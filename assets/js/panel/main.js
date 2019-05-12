e470f27fb4efb4e21d094f33d49a9631f0dc4ccb
iosdeveloper <iosdeveloper@iosdevelopers-iMac.local>
2019-05-12 20:26:02 +0430

    [ FEAT ] Responsive Menu

--- assets/js/panel/main.js
+++ assets/js/panel/main.js
@@ -1,7 +1,41 @@
+// Pre loader
+$(window).on("load", function() {
+    setTimeout(function() {
+        $(".preloader-backdrop").fadeOut(200)
+    }, 0);
+}); 
+
+// Header nav
 $(document).ready(function () {
     var headerNavToggler = $("#header-nav-toggler");
 
     headerNavToggler.on('click', function () {
         $('body').toggleClass('header-nav-hidden');
     });
-})
+});
+
+
+
+var nav_menu_width = $(".header-menu").outerWidth();
+$(window).on("load resize scroll", function() {
+    $(this).width() <= nav_menu_width ? $("body").hasClass("drawer-menu") || $("body").addClass("drawer-menu") : ($("body").removeClass("drawer-menu"),
+    $("#header-nav").hasClass("shined") && closeShined())
+});
+$(function (){
+    $("#header-nav-toggler").click(function() {
+        $("body").hasClass("drawer-menu") ? $("#header-nav").backdrop() : $("body").toggleClass("header-nav-hidden")
+    })
+});
+
+// functions
+
+$.fn.backdrop = function() {
+    return $(this).toggleClass("shined"),
+    $("body").toggleClass("has-backdrop"),
+    $(this);
+}
+
+function closeShined() {
+    $("body").removeClass("has-backdrop"),
+    $(".shined").removeClass("shined")
+}
